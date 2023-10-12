e910dc626104ffb9c23737283d4423a642c34195
Cgyulas0525 <cseszneki.gyula@gmail.com>
2023-10-12 18:39:38 +0200

    UJ-17 Enums Description Attribute

--- app/Enums/ActiveEnum.php
+++ app/Enums/ActiveEnum.php
@@ -7,12 +7,18 @@ use ArchTech\Enums\Options;
 use ArchTech\Enums\Names;
 use ArchTech\Enums\Values;
 use ArchTech\Enums\From;
+use ArchTech\Enums\Metadata;
+use ArchTech\Enums\Meta\Meta;
+use App\Enums\MetaProperties\OrderType\{Description};
 
+#[Meta(Description::class)]
 enum ActiveEnum: string
 {
-    use InvokableCases, Options, Names, Values, From;
+    use InvokableCases, Options, Names, Values, From, Metadata;
 
+    #[Description('Inaktív tétel')]
     case INACTIVE = 'inaktív';
+    #[Description('Aktív tétel')]
     case ACTIVE = 'aktív';
 
 }
