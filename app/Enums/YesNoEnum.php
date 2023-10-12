89ba8f327bf73a913d0c665ce239aaee053e8b02
Cgyulas0525 <cseszneki.gyula@gmail.com>
2023-10-12 18:39:38 +0200

    UJ-17 Enums Description Attribute

--- app/Enums/YesNoEnum.php
+++ app/Enums/YesNoEnum.php
@@ -7,10 +7,14 @@ use ArchTech\Enums\Options;
 use ArchTech\Enums\Names;
 use ArchTech\Enums\Values;
 use ArchTech\Enums\From;
+use ArchTech\Enums\Metadata;
+use ArchTech\Enums\Meta\Meta;
+use App\Enums\MetaProperties\OrderType\{Description};
 
+#[Meta(Description::class)]
 enum YesNoEnum: string
 {
-    use InvokableCases, Options, Names, Values, From;
+    use InvokableCases, Options, Names, Values, From, Metadata;
 
     case NO = 'Nem';
     case YES = 'Igen';
