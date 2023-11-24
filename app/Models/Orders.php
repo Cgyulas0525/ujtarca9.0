a27245ef368723977def4344a02a4ae317080056
Cgyulas0525 <cseszneki.gyula@gmail.com>
2023-11-24 08:22:56 +0100

    UJ-19 refactored

--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -35,10 +35,7 @@ class Orders extends Model
 
     const CREATED_AT = 'created_at';
     const UPDATED_AT = 'updated_at';
-
-
     protected $dates = ['deleted_at'];
-
 
     public $fillable = [
         'ordernumber',
