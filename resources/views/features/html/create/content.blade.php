d4ab901c337acc69728760aea8c8d4306bcac506
Cgyulas0525 <cseszneki.gyula@gmail.com>
2024-02-09 13:26:43 +0100

    UJ-21 teszt

--- resources/views/features/html/create/content.blade.php
+++ resources/views/features/html/create/content.blade.php
@@ -1,5 +1,5 @@
 @section('content')
-    @include('html.content-header', ['title' => __('Jellemző')])
+    @include('html.content-header', ['title' => __('Jellemző aaaaa')])
     <div class="content px-3">
         @include('adminlte-templates::common.errors')
         @include('features.html.create.card')
