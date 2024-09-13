7d58ead721d94b669ee6f4bfaf09b664f06ab92c
Cgyulas0525 <cseszneki.gyula@gmail.com>
2024-09-13 18:29:09 +0200

    UJ-22-Media-and-TableUpdateOrInsert-modifying

--- app/Services/Stacked/PeriodAverageService.php
+++ app/Services/Stacked/PeriodAverageService.php
@@ -7,15 +7,13 @@ use \Carbon\Carbon;
 
 class PeriodAverageService
 {
-    public static function weekPeriodResultAverage($howmany, $withweek): array
+    public static function weekPeriodResultAverage(int $howmany, int $withweek): array
     {
         $date = new \DateTime;
         $resultArray = ['revenue' => 0, 'spend' => 0, 'result' => 0, 'number' => 0];
         foreach (Weekstacked::where('id', '<', Weekstacked::get()->last()->id)->orderBy('year', 'desc')->orderBy('week', 'desc')->get()->take($howmany) as $data) {
             $date = $date->setISODate($data->year, $data->week);
             $week = Carbon::parse($date->format('Y-m-d'));
-
-
             if ($week->weekOfMonth == $withweek) {
                 $resultArray['result'] += $data->result;
                 $resultArray['revenue'] += $data->revenue;
